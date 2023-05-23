e7ca53027580107d76efeb7708248ae3e7488444
Luoxy <lxy@ctgu.edu.cn>
2023-05-23 23:03:09 +0800

    DeepConCluster

--- DeepConCluster.py
+++ DeepConCluster.py
@@ -1,36 +1,34 @@
 import matplotlib.pyplot as plt
 import tensorflow as tf
-from tf_fits.image import image_decode_fits
 from tensorflow.keras import layers, Sequential
 import numpy as np
 import pandas as pd
 import os
 import tqdm
-from deep_clustering import BasicBlock
-from model_ulti.get_tfrecord import scale_tf, tf_serialize_example
-from deep_clustering import load_dataset
 from scipy.spatial.distance import pdist, squareform, cdist
 from sklearn.metrics.cluster import normalized_mutual_info_score
 from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score, accuracy_score
 from ConstrainedKMeans.src.ConstrainedKMeans import ConstrainedKMeans as CKM
-# import tensorflow as tf
-np.random.seed(1)
-# tf.random.set_seed(1)
 
+from model_ulti.make_pic import get_p_distribution, plot_train_result, plot_test_acc_nmi, \
+    plot_test_acc_reassign, decisionGraph
+from deep_clustering import load_dataset, BasicBlock
+
+
+np.random.seed(1)
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 gpus = tf.config.list_physical_devices(device_type='GPU')
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
-# 1.TF_CPP_MIN_LOG_LEVEL = 1 //默认设置，为显示所有信息
-# 2.TF_CPP_MIN_LOG_LEVEL = 2 //只显示error和warining信息
-# 3.TF_CPP_MIN_LOG_LEVEL = 3 //只显示error信息
+    # 1.TF_CPP_MIN_LOG_LEVEL = 1 //默认设置，为显示所有信息
+    # 2.TF_CPP_MIN_LOG_LEVEL = 2 //只显示error和warining信息
+    # 3.TF_CPP_MIN_LOG_LEVEL = 3 //只显示error信息
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 
 loss_object = tf.keras.losses.BinaryCrossentropy(from_logits=True)
-# 定义ResNet
-class ResNet_dc(tf.keras.Model):
 
+class ResNet_dc(tf.keras.Model):
     def build_resblock(self, filter_num, blocks, stride=1):
         res_blocks = Sequential()
         res_blocks.add(BasicBlock(filter_num, stride))
@@ -68,7 +66,6 @@ class ResNet_dc(tf.keras.Model):
 
         return x, extrect_feature
 
-
 def loss(model, x, y, weight=False):
     # training=training is needed only if there are layers with different
     # behavior during training versus inference (e.g. Dropout).
@@ -86,80 +83,12 @@ def loss(model, x, y, weight=False):
 
     return loss0
 
-
 def grad(model, x, y, weight=False):
 
     with tf.GradientTape() as tape:
         loss_value = loss(model, x, y, weight=weight)
     grads = tape.gradient(loss_value, model.trainable_variables)
     return loss_value, grads
-
-
-def load_dataset1(data_set_path):
-    file_list = os.listdir(data_set_path)
-    file_list1 = [os.path.join(data_set_path, item) for item in file_list]
-    file_label1 = np.array([item.split('_')[0] for item in file_list], np.int32)
-
-    labels = tf.constant(file_label1, tf.int32, shape=(len(file_list1)))  # ==> 3x1 tensor
-    labels_dataset = tf.data.Dataset.from_tensor_slices(labels)
-    features = tf.constant(file_list1, tf.string, shape=(len(file_list1)))  # ==> 3x2 tensor
-    features_dataset = tf.data.Dataset.from_tensor_slices(features)
-
-    dataset = tf.data.Dataset.zip((features_dataset, labels_dataset))
-    dataset = dataset.shuffle(buffer_size=len(file_list1))
-    dataset = dataset.map(tf_serialize_example)
-
-    return dataset, labels_dataset, file_label1
-
-
-def preprocess_x(x):
-
-    header = 0
-    img = tf.io.read_file(x[0])
-    img = image_decode_fits(img, header)
-    img = img.numpy()
-
-    scale = 30
-    temp1 = scale_tf(img, scale)
-    x = tf.cast(temp1, tf.float32)
-    x = tf.expand_dims(x, -1)
-
-    return x
-
-
-def tf_serialize_example_x(x):
-    x = tf.py_function(preprocess_x, [x], tf.float32)      # the return type is `tf.string`.# The result is a scalar
-    return x
-
-
-def decisionGraph(feature_all, show=False):
-    if show:
-        feature_all_n = (feature_all - feature_all.min(0)) / (feature_all.max(0) - feature_all.min(0) + 0.0001)
-        dist_array = pdist(feature_all_n, metric='euclidean')
-        dist_m = squareform(dist_array, 'tomatrix')
-        NE = dist_m.shape[0]
-        sort_distrow = np.sort(dist_array)
-        dc = sort_distrow[int(NE * (NE - 1) / 2 * 0.02)]
-        rho = np.sum(np.exp(-1 * (dist_m / dc) ** 2), axis=0) - 1
-
-        delta = np.zeros([NE], np.float32)
-        INN = np.inf * np.ones_like(delta, np.int32)
-        rhoRho = np.argsort(-1 * rho)   # 降序的索引
-        for rho_i in tqdm.tqdm(range(1, NE)):
-            delta[rhoRho[rho_i]] = max(dist_m[rhoRho[rho_i], ...])
-            for rho_j in range(rho_i):
-                if delta[rhoRho[rho_i]] > dist_m[rho_i, rho_j]:
-                    delta[rhoRho[rho_i]] = dist_m[rho_i, rho_j]
-                    INN[rhoRho[rho_i]] = rhoRho[rho_j]
-        delta[rhoRho[0]] = max(delta)
-        INN[rhoRho[0]] = 0
-
-        plt.figure()
-        plt.scatter(rho, delta+0.01)
-        # plt.xscale('log')
-        # plt.yscale('log')
-        plt.show()
-
 
 def re_sign_label(cen_new, cen_old, label_new, label_old):
     # update cluster --> y_pseudo
@@ -180,174 +109,6 @@ def re_sign_label(cen_new, cen_old, label_new, label_old):
     print('%s: %.1f, %.1f' % (aa, NMI_0, NMI_1))
     return cen_new, label
 
-
-def plot_train_result(Result, savepath=None):
-    # colnames = ['acc_kmeans', 'recall_train', 'precision_train', 'f1_train', 'loss_value_train', 'accuracy_train',
-    #             'recall_test', 'precision_test', 'f1_test', 'loss_value_test', 'accuracy_test']
-    acc_kmeans = Result['acc_kmeans'].values
-
-    recall_train = Result['recall_train'].values
-    precision_train = Result['precision_train'].values
-    f1_train = Result['f1_train'].values
-    loss_value_train = Result['loss_value_train'].values
-    accuracy_train = Result['accuracy_train'].values
-
-    recall_test = Result['recall_test'].values
-    precision_test = Result['precision_test'].values
-    f1_test = Result['f1_test'].values
-    loss_value_test = Result['loss_value_test'].values
-    accuracy_test = Result['accuracy_test'].values
-
-    # 画损失函数图
-    fig = plt.figure(figsize=(10, 6))
-    # 坐标轴的刻度设置向内(in)或向外(out)
-    plt.rcParams['xtick.direction'] = 'in'
-    plt.rcParams['ytick.direction'] = 'in'
-
-    ax1 = fig.add_subplot(221)
-    ax1.tick_params(top=True, right=True)
-    ax1.plot(loss_value_train, label='Train loss')
-    ax1.plot(loss_value_test, label='Test loss')
-    ax1.set_xlabel('Epochs')
-    ax1.set_ylabel('Loss')
-    ax1.legend()
-
-    # subplot acc_train
-    ax3 = fig.add_subplot(222)
-    ax3.tick_params(top=True, right=True)
-    ax3.plot(accuracy_train, label='Train accuracy')
-    ax3.plot(accuracy_test, label='Test accuracy')
-    ax3.plot(acc_kmeans, label='K-Means accuracy')
-    ax3.set_xlabel('Epochs')
-    ax3.set_ylabel('Accuracy')
-    ax3.legend()
-
-    # subplot acc_train
-    ax2 = fig.add_subplot(223)
-    ax2.tick_params(top=True, right=True)
-    ax2.plot(recall_train, label='Train recall')
-    ax2.plot(recall_test, label='Test recall')
-    ax2.set_xlabel('Epochs')
-    ax2.set_ylabel('Recall')
-    ax2.legend()
-
-    # subplot acc_train
-    ax4 = fig.add_subplot(224)
-    ax4.tick_params(top=True, right=True)
-    ax4.plot(precision_train, label='Train precision')
-    ax4.plot(precision_test, label='Test precision')
-    ax4.plot(f1_train, label='Train f1')
-    ax4.plot(f1_test, label='Test f1')
-    ax4.set_xlabel('Epochs')
-    ax4.set_ylabel('Precision & f1')
-    ax4.legend()
-
-    plt.tight_layout()
-    if savepath is None:
-        plt.show()
-    else:
-        fig.savefig(savepath, dpi=600, format='png')
-        plt.close(fig)
-
-
-def plot_test_acc_nmi(Result, savepath=None):
-    # colnames = ['acc_kmeans', 'recall_train', 'precision_train', 'f1_train', 'loss_value_train', 'accuracy_train',
-    #             'recall_test', 'precision_test', 'f1_test', 'loss_value_test', 'accuracy_test', 'NMI_t_t1']
-    recall_test = Result['recall_test'].values
-    precision_test = Result['precision_test'].values
-    f1_test = Result['f1_test'].values
-    loss_value_test = Result['loss_value_test'].values
-    accuracy_test = Result['accuracy_test'].values
-    NMI_t_label = Result['NMI_t_label'].values
-    NMI_t_t1 = Result['NMI_t_t1'].values
-    resign_num_ratio = Result['cluster_resign_num'].values / Result['cluster_resign_num'].values.max()
-
-    # 画损失函数图
-    fig = plt.figure(figsize=(10, 3))
-    # 坐标轴的刻度设置向内(in)或向外(out)
-    plt.rcParams['xtick.direction'] = 'in'
-    plt.rcParams['ytick.direction'] = 'in'
-
-    ax1 = fig.add_subplot(131)
-    ax1.tick_params(top=True, right=True)
-    ax1.plot(NMI_t_label, label='MNI t / labels')
-    ax1.plot(NMI_t_t1, label='NMI t-1 / t')
-    ax1.plot(resign_num_ratio, label='Reassignment ratio')
-    ax1.set_xlabel('Epochs')
-    ax1.set_ylabel('MNI Test Dataset')
-    ax1.legend()
-
-    # subplot acc_train
-    ax2 = fig.add_subplot(132)
-    ax2.tick_params(top=True, right=True)
-    ax2.plot(recall_test, label='Test recall')
-    ax2.plot(precision_test, label='Test precision')
-
-    ax2.set_xlabel('Epochs')
-    ax2.set_ylabel('Evaluation Indicators')
-    ax2.legend()
-
-    ax3 = fig.add_subplot(133)
-    ax3.tick_params(top=True, right=True)
-    ax3.plot(f1_test, label='Test F1 score')
-    ax3.plot(accuracy_test, label='Test accuracy')
-
-    ax3.set_xlabel('Epochs')
-    ax3.set_ylabel('Evaluation Indicators')
-    ax3.legend()
-
-    plt.tight_layout()
-    if savepath is None:
-        plt.show()
-    else:
-        fig.savefig(savepath, dpi=600, format='png')
-        plt.close(fig)
-
-
-def plot_test_acc_reassign(Result, savepath=None):
-    # colnames = ['acc_kmeans', 'recall_train', 'precision_train', 'f1_train', 'loss_value_train', 'accuracy_train',
-    #             'recall_test', 'precision_test', 'f1_test', 'loss_value_test', 'accuracy_test', 'NMI_t_t1']
-
-    f1_test = Result['f1_test'].values
-    NMI_t_label = Result['NMI_t_label'].values
-    NMI_t_t1 = Result['NMI_t_t1'].values
-    # 画损失函数图
-    fig = plt.figure(figsize=(10, 3))
-    # 坐标轴的刻度设置向内(in)或向外(out)
-    plt.rcParams['xtick.direction'] = 'in'
-    plt.rcParams['ytick.direction'] = 'in'
-
-    ax1 = fig.add_subplot(131)
-    ax1.tick_params(top=True, right=True)
-    ax1.plot(NMI_t_label)
-    ax1.set_xlabel('Epochs')
-    ax1.set_ylabel('NMI t / labels')
-
-    # subplot acc_train
-    ax2 = fig.add_subplot(132)
-    ax2.tick_params(top=True, right=True)
-
-    ax2.plot(NMI_t_t1)
-    ax2.set_xlabel('Epochs')
-    ax2.set_ylabel('NMI t-1 / t')
-
-    ax3 = fig.add_subplot(133)
-    ax3.tick_params(top=True, right=True)
-    ax3.plot(f1_test)
-    ax3.set_ylim([0.5, 1])
-    # ax3.plot(accuracy_test, label='Test accuracy')
-
-    ax3.set_xlabel('Epochs')
-    ax3.set_ylabel('$F_1$')
-
-    plt.tight_layout()
-    if savepath is None:
-        plt.show()
-    else:
-        fig.savefig(savepath, dpi=600, format='png')
-        plt.close(fig)
-
-
 def get_r_p_f1_loss(y_true_all, y_pred_all):
 
     recall = recall_score(y_true_all, y_pred_all.argmax(1))
@@ -358,12 +119,10 @@ def get_r_p_f1_loss(y_true_all, y_pred_all):
     accuracy = accuracy_score(y_true_all, y_pred_all.argmax(1))  # 模型预测的准确率
     return recall, precision, f1, confusion, loss_value, accuracy
 
-
 def save_paraset(result_path_para, para_dict):
     with open(result_path_para, 'w') as ww:
         for key, values in zip(para_dict.keys(), para_dict.values()):
             print('{}: {}'.format(key, values), file=ww)
-
 
 def get_stable_samples_idx(feature_all, pseudo_label, ratio, cluster_id=1):
     """
@@ -387,7 +146,6 @@ def get_stable_samples_idx(feature_all, pseudo_label, ratio, cluster_id=1):
     idx_1_ = np.where(cdistance <= cd_1_th)[0]
 
     return idx[idx_1_]
-
 
 def get_stable_pseudo_label(feature_all, pseudo_label, can_change, ratio=0.5):
     """
@@ -434,7 +192,6 @@ def get_stable_pseudo_label(feature_all, pseudo_label, can_change, ratio=0.5):
 
     return idx
 
-
 def get_dataset(pseudo_label, x_dataset, index_dataset, batch_size=1000):
     pseudo_label_hot = tf.keras.utils.to_categorical(pseudo_label, num_classes=2)
     pseudo_labels_dataset = tf.data.Dataset.from_tensor_slices(pseudo_label_hot)
@@ -442,55 +199,6 @@ def get_dataset(pseudo_label, x_dataset, index_dataset, batch_size=1000):
     dataset_new = dataset_new.batch(batch_size=batch_size)
 
     return dataset_new
-
-
-def get_p_distribution(y_true_label, y_test_pred, savepath):
-    # save_path = 'result/R2_feature_256_block_1_test_seed_0507_ratio_04_01'
-    # savepath = 'result/R2_feature_256_block_1_test_seed_0507_ratio_04_01/matrix.png'
-    # os.makedirs(save_path, exist_ok=True)
-    TP = []
-    FN = []
-    FP = []
-    TN = []
-    for true_label, y_pred in zip(y_true_label, y_test_pred):
-        pred_label = np.argmax(y_pred)
-
-        if true_label == 1 and pred_label == 1:
-            TP.append(y_pred)
-        if true_label == 1 and pred_label == 0:
-            FN.append(y_pred)
-        if true_label == 0 and pred_label == 1:
-            FP.append(y_pred)
-        if true_label == 0 and pred_label == 0:
-            TN.append(y_pred)
-
-    TP = np.array(TP)
-    TN = np.array(TN)
-    FP = np.array(FP)
-    FN = np.array(FN)
-
-    fig = plt.figure(figsize=[10, 6])
-    ide_style = ['TP', 'FP', 'FN', 'TN']
-
-    for i, item in enumerate([TP, FP, FN, TN]):
-        ax1 = fig.add_subplot(2, 2, i + 1)
-        ax1.hist(y_test_pred[:, 1], 38)
-        ax1.tick_params(top=True, right=True)
-        if item.shape[0] > 0:
-            if ide_style[i] == 'TP':
-                data = item[:, 1]
-            if ide_style[i] == 'FP':
-                data = item[:, 1]
-            if ide_style[i] == 'FN':
-                data = item[:, 1]
-            if ide_style[i] == 'TN':
-                data = item[:, 1]
-            ax1.hist(data, 20, label='%s\nnum=%d' % (ide_style[i], data.shape[0]))
-        else:
-            ax1.hist(y_test_pred[:, 1], 38, label='%s\nnum=%d' % (ide_style[i], 0))
-        ax1.legend()
-    fig.savefig(savepath, dpi=600, format='png')
-    plt.close(fig)
 
 
 if __name__ == '__main__':
@@ -502,7 +210,7 @@ if __name__ == '__main__':
         # model_path = r'model/R16_feature_256_block_1_test_seed_0507_ratio_050_01/epoch_model/model_025.h5'
         para_dict = {'model_path': model_path, 'n_clusters': 2, 'num_epochs': 5, 'batch_size': 1000,
                      'feature_num': feat_num, 'block_num': 1, 'learning rate': 0.005, 'region': 'R16',
-                     'savepath_log': 'fine_tuning_%03d_01' % (100*ratio), 'train_num': 5,
+                     'savepath_log': 'fine_tuning_%03d_01rgerg' % (100*ratio), 'train_num': 5,
                      'model': 'R2'}
 
         if para_dict['region'] == 'M16':
--- deep_clustering.py
+++ deep_clustering.py
@@ -1,24 +1,9 @@
 import tensorflow as tf
-from model_ulti.get_tfrecord import fits_read, preprocess, tf_serialize_example
 from tensorflow.keras import layers, Sequential
 import numpy as np
 import os
-import tqdm
-from resnet_3 import ResNet as resnet
-import pre_data_1 as pre_data
 from sklearn.cluster import KMeans
-from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score, accuracy_score
-from sklearn.metrics.cluster import entropy, normalized_mutual_info_score
-
-# tf.config.experimental_run_functions_eagerly(True)
-# np.random.seed(1)
-
-# os.environ["CUDA_VISIBLE_DEVICES"] = '0'
-# gpus = tf.config.list_physical_devices(device_type='GPU')
-# for gpu in gpus:
-#     tf.config.experimental.set_memory_growth(gpu, True)
-
-
+from model_ulti.get_tfrecord import tf_serialize_example
 # 定义Basic Block
 class BasicBlock(layers.Layer):
     def __init__(self, filter_num, stride=1):
@@ -46,7 +31,6 @@ class BasicBlock(layers.Layer):
         out = layers.add([out, identity])
         out = tf.nn.relu(out)
         return out
-
 
 # 定义ResNet
 class ResNet_dc(tf.keras.Model):
@@ -105,87 +89,6 @@ class ResNet_dc(tf.keras.Model):
         ans = tf.keras.losses.binary_crossentropy(Y_test_pred_hot, x)
         return ans
 
-
-def load_model_weights(model, model_h5='model/model.h5'):
-    model_old = resnet([1])
-    model_old.build(input_shape=(None, 30, 30, 30, 1))
-    model_old.load_weights(model_h5)
-    for i in range(3):
-        model.layers[i].set_weights(model_old.layers[i].get_weights())
-
-    for i in range(3):
-        model.layer3.layers[i].set_weights(model_old.layer3.layers[i].get_weights())
-
-    model.layer4.set_weights(model_old.layer3.layers[3].get_weights())
-    return model
-
-
-class WeightedSDRLoss(tf.keras.losses.Loss):
-
-    def __init__(self, name='WeightedSDRLoss'):
-        super().__init__(name=name)
-
-    def call(self, y_true, y_pred):
-
-        # print('*****')
-
-        # print('88888')
-        # print(y_pred[0].shape)
-        # print(y_pred[1].shape)
-        # print(y_pred)
-        # print('777777')
-        if y_pred.shape[1] <= 2:
-            return 0
-        # y_true_all = y_true_all.numpy()
-        # print(y_true_all)
-        kmeans = KMeans(n_clusters=2, random_state=0).fit(y_pred)
-        Y_test_pred_hot = tf.keras.utils.to_categorical(kmeans.labels_, num_classes=2)
-        ans = tf.keras.losses.binary_crossentropy(Y_test_pred_hot, y_pred)
-        return ans
-
-
-# loss_object = tf.keras.losses.BinaryCrossentropy(from_logits=True)
-
-
-def loss(feature, y_pred):
-    # training=training is needed only if there are layers with different
-    # behavior during training versus inference (e.g. Dropout).
-    # y_pred, feature = model(x, training=training)
-    kmeans = KMeans(n_clusters=2, random_state=0).fit(feature)
-    #
-    y_pseudo = tf.keras.utils.to_categorical(kmeans.labels_, num_classes=2)   # seudo-labels
-    loss0 = loss_object(y_true=y_pseudo, y_pred=y_pred)
-    # loss1 = tf.keras.losses.binary_crossentropy(y_true_all=y_true_all, y_pred=y_pred)
-    # epoch_loss_avg = tf.keras.metrics.Mean()
-    # epoch_loss_avg.update_state(loss0)
-    # loss0 = epoch_loss_avg.result()
-    return loss0
-
-
-def grad(model, feature_all, y_pred_all):
-
-    with tf.GradientTape() as tape:
-        loss_value = loss(feature_all, y_pred_all)
-        print(1)
-    grads = tape.gradient(loss_value, model.trainable_variables)
-    return loss_value, grads
-
-
-def loss1(model, x, targets, training):
-    # training=training is needed only if there are layers with different
-    # behavior during training versus inference (e.g. Dropout).
-    y_, feature = model(x, training=training)
-    kmeans = KMeans(n_clusters=2, random_state=0).fit(feature)
-    Y_test_pred_hot = tf.keras.utils.to_categorical(kmeans.labels_, num_classes=2)
-    return loss_object(y_true=Y_test_pred_hot, y_pred=y_)
-
-
-def grad1(model, x, targets):
-    with tf.GradientTape() as tape:
-        loss_value = loss1(model, x, targets, training=False)
-    return loss_value, tape.gradient(loss_value, model.trainable_variables)
-
-
 def load_dataset(data_set_path, batch_size=3000):
     temp = os.listdir(data_set_path)
     file_list1 = [os.path.join(data_set_path, item) for item in temp]
@@ -200,104 +103,6 @@ def load_dataset(data_set_path, batch_size=3000):
     dataset = dataset.map(tf_serialize_example)
     dataset = dataset.batch(batch_size=batch_size)
     return dataset
-
-
-def load_dataset_M16_500():
-    data_set_path = '/home/data/clumps_share/real_data_cube_set/M16_clumps/train/'
-    temp = [item for item in os.listdir(data_set_path) if not item.split('_')[-2].isalpha()]
-    sample_num = len(temp)
-
-    file_list1 = [os.path.join(data_set_path, item) for item in temp]
-    file_label1 = np.array([item.split('_')[0] for item in temp], np.int32)
-    features = tf.constant(file_list1, tf.string, shape=(len(file_list1), 1))  # ==> 3x2 tensor
-    labels = tf.constant(file_label1, shape=(len(file_list1)))  # ==> 3x1 tensor
-
-    features_dataset = tf.data.Dataset.from_tensor_slices(features)
-    labels_dataset = tf.data.Dataset.from_tensor_slices(labels)
-    dataset = tf.data.Dataset.zip((features_dataset, labels_dataset))
-    dataset = dataset.shuffle(buffer_size=len(file_list1))
-    dataset = dataset.map(tf_serialize_example)
-    dataset = dataset.batch(batch_size=sample_num)
-    return dataset, sample_num
-
-
-def main():
-    num_epochs = 500
-    batch_size = 3000
-    data_set_path = r'/home/data/clumps_share/real_data_cube_set/R2_R16_clumps'
-    train_pseudo_label = True
-    load_trained_model = True
-
-    model = ResNet_dc([1])
-    model.build(input_shape=(None, 30, 30, 30, 1))
-    if load_trained_model:
-        model_h5 = 'model/deep_clustering_838.h5'
-        # model.load_weights(model_h5)
-        load_model_weights(model, model_h5=model_h5)
-    print('model over')
-    dataset = load_dataset(data_set_path, batch_size=batch_size)
-    print('x_dataset over')
-    learning_rate = tf.keras.optimizers.schedules.ExponentialDecay(initial_learning_rate=0.01,
-                                                                   decay_rate=0.96,
-                                                                   decay_steps=10000)
-    optimizer = tf.keras.optimizers.SGD(learning_rate=learning_rate)
-
-    train_loss_results = []
-    train_accuracy_results = []
-
-    epoch_loss_avg = tf.keras.metrics.Mean()
-    epoch_accuracy = tf.keras.metrics.BinaryAccuracy()
-    for epoch in range(num_epochs):
-        # Training loop - using batches of 32
-        with tf.GradientTape() as tape:
-            train_x_, y_true_label = next(iter(dataset))
-            y_pred_all, feature_all = model(train_x_, training=True)
-            print('*')
-            for train_x_, train_y_ in tqdm.tqdm(dataset):
-                y_pred, feature = model(train_x_, training=True)
-                feature_all = tf.concat([feature_all, feature], axis=0)
-                y_pred_all = tf.concat([y_pred_all, y_pred], axis=0)
-                y_true_label = tf.concat([y_true_label, train_y_], axis=0)
-
-            if train_pseudo_label:
-                kmeans = KMeans(n_clusters=2).fit(feature_all)
-                kmeans_labels = kmeans.labels_
-                y_pseudo = tf.keras.utils.to_categorical(kmeans_labels, num_classes=2)
-                y_label = y_pseudo
-                loss_value = tf.keras.losses.BinaryCrossentropy(from_logits=True)(y_true=y_pseudo, y_pred=y_pred_all)
-            else:
-                y_label = y_true_label
-                loss_value = tf.keras.losses.BinaryCrossentropy(from_logits=True)(y_true=y_label, y_pred=y_pred_all)
-        y_true_label = y_true_label.numpy().squeeze()
-        NMI = normalized_mutual_info_score(np.argmax(y_true_label, 1), np.argmax(y_pred_all, 1))
-        grads1 = tape.gradient(loss_value, model.trainable_variables)
-        optimizer.apply_gradients(zip(grads1, model.trainable_variables))
-        epoch_loss_avg.update_state(loss_value)  # Add current batch loss
-        epoch_accuracy.update_state(y_true_label, y_pred_all)
-
-        # End epoch
-        train_loss_results.append(epoch_loss_avg.result())
-        train_accuracy_results.append(epoch_accuracy.result())
-
-        print("Epoch {:03d}: Loss: {:.3f}; Accuracy: {:.3f}; NMI_t_t1={}; cluster_n={} grads={}".format(epoch,
-                                                                                                   epoch_loss_avg.result(),
-                                                                                                   epoch_accuracy.result(),
-                                                                                                   NMI,
-                                                                                                   kmeans_labels.sum(),
-                                                                                                   grads1[0][:1, 0, 0,
-                                                                                                   0, 0].numpy()[0]))
-
-    test_x, test_y = pre_data.per_fits_data(path='data/test/')
-    test_y_ = tf.keras.utils.to_categorical(test_y, num_classes=2)
-    test_x = tf.expand_dims(test_x, -1)
-    test_x = tf.cast(test_x, dtype=tf.float32)
-    pre_y = model(test_x, training=False)
-    pre_y_ = np.argmax(pre_y[0], axis=1)
-    print(accuracy_score(test_y, pre_y_))
-
-    feature = pre_y[1].numpy()
-    kmeans = KMeans(n_clusters=2, random_state=0).fit(feature)
-    print(accuracy_score(test_y, kmeans.labels_))
 
 
 if __name__ == '__main__':
